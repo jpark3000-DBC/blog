6c96def0df6c626be0c6a21c02a94bd3c6f7f151
Apprentice <apprentice@dbc-chicago-54.local>
2014-01-30 13:15:06 -0600

    added route and view for single post

--- app/controllers/posts.rb
+++ app/controllers/posts.rb
@@ -6,5 +6,11 @@ post "/create_post" do
   Post.create(title: params[:post][:title],
               body: params[:post][:body],
               author: params[:post][:author] )
+
   redirect to('/')
+end
+
+get "/post/:post_id" do
+  @post = Post.find(params[:post_id])
+  erb :single_post
 end
