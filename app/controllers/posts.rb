ef67d9287f0a93eac84269a3c5957515d2346bf5
Apprentice <apprentice@dbc-chicago-54.local>
2014-01-30 13:47:43 -0600

    added editing functionality for posts

--- app/controllers/posts.rb
+++ app/controllers/posts.rb
@@ -14,3 +14,19 @@ get "/post/:post_id" do
   @post = Post.find(params[:post_id])
   erb :single_post
 end
+
+get "/edit/:post_id" do
+  @post = Post.find(params[:post_id])
+  erb :edit_post
+end
+
+post "/edit/:post_id" do
+  # puts "#{params[:post_id]}"
+  # puts "#{params[:post][:title]}"
+  Post.update(params[:post_id],
+              title:  params[:post][:title],
+              body:   params[:post][:body],
+              author: params[:post][:author])
+  redirect to("/post/#{params[:post_id]}")
+end
+
